849e0c376a69bb18cd48b70f47dda3eafe79166c
hpy <2924418812@qq.com>
2025-05-14 18:53:10 +0800

    ocr&segment&translate功能

--- java/demo/src/main/java/com/example/demo/controller/DeepfillController.java
+++ java/demo/src/main/java/com/example/demo/controller/DeepfillController.java
@@ -10,11 +10,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
---
+++ java/demo/src/main/java/com/example/demo/service/TranslateService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.pojo.OcrBean;
+
+public interface TranslateService {
+    public boolean runTransScript(OcrBean ocrBean);
+}
