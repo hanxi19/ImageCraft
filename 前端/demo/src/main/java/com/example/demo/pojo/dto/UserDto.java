24f7fc297d2d6b1558f261f5f47a89cadf7824ad
hanxi <1720492944@qq.com>
2025-06-02 20:15:42 +0800

    修改登录注册功能的bug

---
+++ 前端/demo/src/main/java/com/example/demo/pojo/dto/UserDto.java
@@ -0,0 +1,42 @@
+package com.example.demo.pojo.dto;
+
+public class UserDto {
+
+    //需要新增的字段
+    private String userName;
+    private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
